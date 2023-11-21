283a51a3f85b6432dbcb356f50978e7211b076cd
Vitalii Borshchov <vitalii.v.borshchov@gmail.com>
2023-11-21 12:39:59 +0200

    feat: created bd connection

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { errorHandlerMiddleware } from './middleware/error-handler'
 import { notFoundMiddleware } from './middleware/not-found'
 import { swaggerSpec } from './swagger/swagger'
 import rootRouter from './rootRoutes'
+import { connectDB } from './db/connect'
 
 const PORT = process.env.PORT ?? 3000
 const app: Express = express()
 
+app.use(express.json())
 // routes
 app.use('/', rootRouter)
 
@@ -15,11 +17,16 @@ app.use('/', rootRouter)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 // middleware
+app.use(express.json())
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
 const start = async (): Promise<void> => {
   try {
+    await connectDB(
+      process.env.MONGO_URL ??
+        'mongodb+srv://vitaliivborshchov:12345@epam-nodejs-course-prac.gxfdvhd.mongodb.net/MoviesDB?retryWrites=true&w=majority'
+    )
     app.listen(PORT, () => {
       console.info(`Server in listening on port ${PORT}\
             \npid: ${process.pid}`)
